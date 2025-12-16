213db0cdfb9a74be228b3ee083f09bb6807fd159
Alvi Dervishaj <alvidervishaj9@gmail.com>
2025-12-16 10:28:15 +0100

    Convert nvim from submodule to regular folder

---
+++ nvim/lua/theprimeagen/lazy/init.lua
@@ -0,0 +1,16 @@
+return {
+
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+    {
+        'brianhuster/live-preview.nvim',
+        dependencies = {
+            -- You can choose one of the following pickers
+            'nvim-telescope/telescope.nvim',
+        },
+    },
+    "eandrju/cellular-automaton.nvim",
+}
+
